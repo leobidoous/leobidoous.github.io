c6d065cd784be7fea6f2ea39366e9422b2de8b6a
Leonardo Emilly Dias Santos Bidó <leobidoous@icloud.com>
2020-01-27 09:23:49 -0300

    update project

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn omni_mobile_backend.wsgi --log-file -
+web: python manage.py runserver 0.0.0.0:$PORT
--- omni_mobile_backend/settings.py
+++ omni_mobile_backend/settings.py
@@ -17,11 +17,12 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "CONST_SECRET_KEY"
+SECRET_KEY = '-uhpfb-=5&l!s0h5sn8*(3a^slvqng24z7b$0yt!a8w+2*$m9#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -122,21 +123,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-# Configurações Heroku
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 ALLOWED_HOSTS = ['*']
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDER_PROTO', 'https')
-
-# auth
-LOGIN_URL = 'core:login'
-LOGIN_REDIRECT_URL = 'core:home'
-LOGOUT_URL = 'core:home'
-# AUTH_USER_MODEL = 'pessoa.User'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 try:
     from .local_settings import *
